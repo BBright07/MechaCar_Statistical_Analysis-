3a55e37f0fcf86fc835a32284890aa6674e9398b
BBright07 <89029747+BBright07@users.noreply.github.com>
2022-01-07 01:20:43 -0500

    final

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -2,7 +2,7 @@
 #load to dplyr package
 library(dplyr)
 
-#Create a new RScript in your R
+#Create MechaCar dataframe
 mechaCarMpg <- read.csv(file='data/MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
 
 #Create linear regression model lm
@@ -12,8 +12,34 @@ summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_cleara
 
 
 ##Deliverabe 2
+#Create Suspension coil dataframe
 susCoil <- read.csv(file='data/Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+
+#Create total_summary of PSI column mean, median, variance and SD
 total_summary <- susCoil %>% summarize(Mean=mean(PSI),
                                           Median=median(PSI),
                                           Variance=var(PSI),
                                           SD=sd(PSI),.groups = 'keep') 
+
+#create lot_summary grouping by lot and getting summary stats
+lot_summary <- susCoil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),
+                                                                     Median=median(PSI),
+                                                                     Variance=var(PSI),
+                                                                     SD=sd(PSI),.groups = 'keep')
+
+##Deliverable 3
+#t.test all manufacturing lots stats. similar to pop mean of 1500 pounds per quare inch
+?t.test()
+t.test(susCoil$PSI,mu=1500)
+
+#2. Use t.test() function 3 more times with subset() to determine if PSI for each manufacturing lot is statistically different from the pop. mean of 1,500 PSI
+
+lot1 <- subset(susCoil, Manufacturing_Lot=="Lot1")
+lot2 <- subset(susCoil, Manufacturing_Lot=="Lot2")
+lot3 <- subset(susCoil, Manufacturing_Lot=="Lot3")
+
+t.test(lot1$PSI,mu=1500)
+t.test(lot2$PSI,mu=1500)
+t.test(lot3$PSI,mu=1500)
+## code help https://github.com/emmanuelmartinezs/MechaCar_Statistical_Analysis/blob/main/MechaCarChallenge.RScript
+
